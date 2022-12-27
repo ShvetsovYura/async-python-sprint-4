fcccd3635da2ac1088b72b79fab41e1649aa4267
Швецов Юрий Валентинович <Shvetsov.Y.Val@sberbank.ru>
2022-12-27 17:33:21 +0300

    Добавлен функционал создания ссылок и редирект

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter, Response, status
+import uuid
+
+from fastapi import APIRouter, Depends, HTTPException, Response, status
 
 from core.utils import read_config
 from db.source import DbSource
 from models.config_models import DbConfig
+from models.request_models import CreateShortLinkModel
 from services.db import DbService
 
 router = APIRouter()
@@ -15,21 +18,36 @@ db_source = DbSource(db_config=DbConfig(**read_config().get('db')))
 db_srv = DbService(db_source=db_source)
 
 
+def get_random_uuid() -> str:
+    return str(uuid.uuid4())[-4:]
+
+
 @router.get('/ping')
 async def get_health_db():
     result = await db_srv.check_db()
-
     return {'db_connection': result}
 
 
-@router.post('/')
-async def create_short_link():
-    pass
+@router.post('/', status_code=status.HTTP_201_CREATED)
+async def create_short_link(
+        link: CreateShortLinkModel,
+        get_url_id: str = Depends(get_random_uuid)    # noqa B008
+):
+    await db_srv.create_link(url_id=get_url_id,
+                             short_url=get_url_id,
+                             original_url=link.original_link)
 
 
-# @router.get('/{url_id}')
-# async def redirect_by_short_link():
-#     pass
+@router.get('/{url_id}', status_code=status.HTTP_307_TEMPORARY_REDIRECT)
+async def redirect_by_short_link(url_id: str, response: Response):
+    result = await db_srv.get_original_by_short(url_id)
+
+    if result and isinstance(result, list):
+        original_link = result[0]['original_url']
+        response.headers['Location'] = original_link
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Link not found')
+
 
 # @router.get('/<url_id>/status?[full-info]&&[max-result=10]&&[offset=0]')
 # async def get_status():
--- src/services/db.py
+++ src/services/db.py
@@ -9,10 +9,14 @@ class DbService:
         self._db_source = db_source
         self._logger = logging.getLogger(f'{__name__}.{self.__class__.__name__}')
 
+    @property
+    def schema(self):
+        return self._db_source.schema
+
     async def check_db(self) -> bool:
         try:
             self._logger.info('Проверка подключения к БД...')
-            _ = await self._fetch_all('select schema_name from information_schema.schemata')
+            _ = await self._execute('select schema_name from information_schema.schemata')
             self._logger.info('подключение к БД успешно')
             return True
         except Exception as e:    # noqa B902
@@ -20,10 +24,25 @@ class DbService:
             self._logger.info('не удалось подклюиться к БД')
             return False
 
-    async def _fetch_all(self, sql: str, *args) -> list:
-        connection_ = await self._db_source.acquire()
-        context = await connection_.run_query(sql, *args)
-        if context.rows:
-            return context.rows
+    async def create_link(self, url_id: str, short_url: str, original_url: str):
 
-        return []
+        stmt = f'INSERT INTO {self.schema}.links(url_id, short_url, original_url) values($1,$2,$3)'
+        await self._execute('START TRANSACTION')
+        result = await self._execute(stmt, url_id, short_url, original_url)
+        await self._execute('COMMIT')
+
+        return result
+
+    async def get_original_by_short(self, url_id: str):
+        stmt = f'SELECT original_url from {self.schema}.links where url_id=$1'
+        return await self._execute(stmt, url_id)
+
+    async def _execute(self, sql: str, *args):
+        connection_ = await self._db_source.acquire()
+        result = await connection_.run_query(sql, *args)
+        if result.rows and result.columns:
+            keys = [col['name'] for col in result.columns]
+            return [dict(zip(keys, row)) for row in result.rows]
+
+        if result.rows_count and not result.columns:
+            return result.rows_count

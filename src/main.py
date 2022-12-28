d68419d4277c8406fd7fdaae460b4f3c3a2b1872
Швецов Юрий Валентинович <Shvetsov.Y.Val@sberbank.ru>
2022-12-28 10:42:31 +0300

    Исправлена мидлваря

--- src/main.py
+++ src/main.py
@@ -1,15 +1,35 @@
 import logging
 import logging.config
 from pathlib import Path
+from typing import Callable
 
 import uvicorn
 import yaml
+from fastapi import FastAPI, Request, Response, status
+from fastapi.middleware import cors
 
-from core.api import create_api
+from api import routes
 from core.utils import read_config
 from models.config_models import WebapiConfig
 
 logger = logging.getLogger(__name__)
+
+config = read_config()
+
+app: FastAPI = FastAPI()
+
+
+@app.middleware('http')
+async def check_client_subnet(request: Request, call_next: Callable):
+
+    if request.client:
+        for subnet_ in config.get('subnet_blacklist', []):
+            if subnet_ in request.client.host:
+                return Response(status_code=status.HTTP_403_FORBIDDEN,
+                                content='Запрещено для этой подсети')
+
+    return await call_next(request)
+
 
 if __name__ == '__main__':
 
@@ -17,8 +37,14 @@ if __name__ == '__main__':
         cfg_ = yaml.safe_load(stream)
         logging.config.dictConfig(cfg_.get('logging'))
 
-    # # asyncio.run(start_webserver()) # noqa E800
+    cfg = WebapiConfig(**config.get('webapi'))
 
-    api = create_api()    # noqa F841 # noqa E800
-    cfg = WebapiConfig(**read_config().get('webapi'))    # noqa E800
-    uvicorn.run('core.api:web_api', port=cfg.port)    # noqa E800
+    app.add_middleware(cors.CORSMiddleware,
+                       allow_credentials=cfg.cors.credentials,
+                       allow_headers=cfg.cors.headers,
+                       allow_methods=cfg.cors.methdods,
+                       allow_origins=cfg.cors.origins)
+
+    app.include_router(routes.router, prefix=cfg.prefix)    # type: ignore
+
+    uvicorn.run('main:app', port=cfg.port)    # noqa E800
